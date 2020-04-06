a471a9cb10a7840f826f0be2ce47d3e1b52ffd28
Markus Stäuble <staeuble@gmail.com>
2020-04-06 13:37:11 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,35 @@
-basic.forever(function () {
-	
+input.onGesture(Gesture.TiltRight, function () {
+    if (gedrueckt == 0) {
+        basic.showLeds(`
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . #
+            `)
+    }
 })
+input.onGesture(Gesture.TiltLeft, function () {
+    if (gedrueckt == 0) {
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    gedrueckt = 1
+})
+let gedrueckt = 0
+gedrueckt = 0
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
